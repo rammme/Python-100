8857e07009856c2cae37e59a4cdcf81cb4a61ebe
rammme <80208912+rammme@users.noreply.github.com>
2021-06-05 20:38:36 +0900

    4

---
+++ code/4.py
@@ -0,0 +1,10 @@
+#문제4: 변수의 타입2
+a = 1 		#출력 : class 'int'
+b = 2.22   	#출력 : class 'float'
+c = 'p'     	#출력 : class 'float'
+d = [1, 2, 3]    	#출력 : class 'float'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
