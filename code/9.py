68d21258ddc3af8bd8b41c25cd00cfc1b5da7ff7
rammme <80208912+rammme@users.noreply.github.com>
2021-06-05 20:39:51 +0900

    9

---
+++ code/9.py
@@ -0,0 +1,11 @@
+#문제9 : sep과 end를 활용한 출력 방법
+
+year = '2019'
+month = '04'
+day = '26'
+hour = '11'
+minute = '34'
+second = '27'
+
+print(year, month, day, sep='/', end=' ') 
+print(hour, minute, second, sep=':' )
